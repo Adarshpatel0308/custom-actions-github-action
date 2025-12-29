7246b5bdc7d6b696fcf3f22c628fac099f404db8
Adarsh Patel <adarshpatel88899@gmail.com>
2025-12-29 00:41:14 -0500

    Added the code for custom javascript and docker actions

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,5 +1,9 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 function run() {
+    // 1. Get some inputs values 
+    // 2. Upload files
     core.notice("Hello from my custom javaScript Action!");
 }
 
